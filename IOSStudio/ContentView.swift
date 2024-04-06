f6082eafe49cb5e553a236c87e90f38c81ab5628
Thomas <thomas.luong.2000@gmail.com>
2024-04-06 14:16:46 +1100

    test

--- IOSStudio/ContentView.swift
+++ IOSStudio/ContentView.swift
@@ -9,7 +9,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
+           Model3D(named: "Immersive", bundle: realityKitContentBundle)
                 .padding(.bottom, 50)
 
             Text("Hello, world!")
