4e39005bd4ac3a1875843ae3b1dd0674d2ff90a7
Isabellebrian <Isabellebrian@gmail.com>
2024-05-07 16:19:03 +1000

    commiting

--- AudioManager.swift
+++ AudioManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import AVFoundation
+
+class AudioManager {
+    static let shared = AudioManager()
+    private var audioPlayer: AVAudioPlayer?
+
+    private init() {}  // Ensures AudioManager cannot be initialized from outside
+
+    func playSound(named soundFileName: String) {
+        guard let url = Bundle.main.url(forResource: soundFileName, withExtension: nil) else {
+            print("Audio file not found")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print("Could not load file:", error)
+        }
+    }
+}
+
