d27f5771314c8ccd2ab8f90654ffdb2beb8607ca
Lachlan <42135069+lachlanelijah@users.noreply.github.com>
2024-05-27 18:28:16 +1000

    added window sizing restrictions

--- IOSStudio/IOSStudioApp.swift
+++ IOSStudio/IOSStudioApp.swift
@@ -8,26 +8,45 @@ struct IOSStudioApp: App {
     
     var body: some Scene {
         WindowGroup (id: "HomeView"){
-            HomeView().environmentObject(scenarioOptions)
+            HomeView()
+                .environmentObject(scenarioOptions)
+                .frame(
+                    minWidth: 400, maxWidth: 400,
+                    minHeight: 400, maxHeight: 400)
         }
+        .windowResizability(.contentSize)
         .defaultSize(CGSize(width: 400, height: 400))
         
         WindowGroup(id: "BriefView") {
-            BriefView().environmentObject(scenarioOptions)
+            BriefView()
+                .environmentObject(scenarioOptions)
                 .environment(viewModel)
+                .frame(
+                    minWidth: 400, maxWidth: 400,
+                    minHeight: 400, maxHeight: 400)
         }
+        .windowResizability(.contentSize)
         .defaultSize(CGSize(width: 400, height: 400))
         
         WindowGroup (id: "ResponseView") {
             ResponseView().environmentObject(feedback)
                 .environment(viewModel)
                 .environmentObject(scenarioOptions)
+                .frame(
+                    minWidth: 520, maxWidth: 520,
+                    minHeight: 400, maxHeight: 400)
         }
+        .windowResizability(.contentSize)
         .defaultSize(CGSize(width: 520, height: 400))
         
         WindowGroup (id: "FeedbackView") {
-            FeedbackView().environmentObject(feedback)
+            FeedbackView()
+                .environmentObject(feedback)
+                .frame(
+                    minWidth: 400, maxWidth: 400,
+                    minHeight: 400, maxHeight: 400)
         }
+        .windowResizability(.contentSize)
         .defaultSize(CGSize(width: 400, height: 400))
         
         ImmersiveSpace(id: "ImmersiveView") {
